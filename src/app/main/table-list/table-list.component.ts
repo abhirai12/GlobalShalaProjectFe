2d54cc18ea2fe1fe99a360fa9c04fc243dff56a3
Abhilash <abhi.aim12.ar@gmail.com>
2021-03-11 13:29:44 +0530

    update

--- src/app/main/table-list/table-list.component.ts
+++ src/app/main/table-list/table-list.component.ts
@@ -11,6 +11,7 @@ export interface UserData {
   last_name: string;
   email: string;
   avatar: string;
+  action:string;
 }
 
 @Component({
@@ -20,14 +21,13 @@ export interface UserData {
 })
 export class TableListComponent implements OnInit {
 
-  displayedColumns: string[] = ['select', 'id', 'first_name', 'last_name', 'email', 'avatar', 'action'];
+  displayedColumns: string[] = ['select', 'id', 'first_name', 'last_name', 'email', 'avatar', 'Action'];
   dataSource: MatTableDataSource<UserData>;
   selection = new SelectionModel<any>(true, []);
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
-    console.log('a', this.dataSource)
     const numRows = this.dataSource?.data.length;
     return numSelected === numRows;
   }
@@ -58,37 +58,37 @@ export class TableListComponent implements OnInit {
       this.dataSource.paginator = this.paginator;
       console.log('b', this.dataSource)
       this.dataSource.sort = this.sort;
-
+      
     });
   }
   ngOnInit() {
 
   }
+
+  // User profile which show in alert box
   profile(id) {
+    let index = this.tableList.data.findIndex(ids => ids.id === id);
     const row = this.tableList.data;
-    console.log(this.tableList)
-    const userId = row[id].id;
-    const frst_name = row[id].first_name;
-    const last_name = row[id].last_name;
-    const email = row[id].email;
+    const userId = row[index].id;
+    const frst_name = row[index].first_name;
+    const last_name = row[index].last_name;
+    const email = row[index].email;
     const alrt = `                 ID : ${userId} , 
                  First Name : ${frst_name}
                  Last Name : ${last_name} , 
                  Email : ${email} , ` 
     alert(alrt);
   }
+
+//  user work fuction
   work(id){
     alert("work in progress!!!!")
   }
+
+  // user contacts function
   con(id){
     alert("contact page are work!!!!")
   }
-  // ngAfterViewInit() {
-  //    this.dataSource.paginator = this.paginator;
-  //    this.dataSource.sort = this.sort;
-  // }
-
-
 }
 
 
